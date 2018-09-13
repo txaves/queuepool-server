185c898570dabeca86c3635a485b92f5c1edffb0
Tiago <tsc@cesar.org.br>
2018-09-13 14:50:56 -0300

    Nodejs version with static Front-end

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var bodyParser = require('body-parser');
+var portDefault = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -11,10 +12,16 @@ res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
-app.use('/', require('./routes'));
+app.use('/api', require('./routes'));
 app.use(express.static('queuepool/'));
 
 var port = process.env.PORT;
-app.set('port', port);
-var server = http.createServer(app);
-server.listen(port);
+if(port) {
+    app.set('port', port);
+    var server = http.createServer(app);
+    server.listen(port);
+} else {
+    app.set('port', portDefault);
+    var server = http.createServer(app);
+    server.listen(portDefault);
+}
